066730c0e647d13a9fc76f8cc1778d89d84abb8e
Ricardo Ríos Arroyo <ricardo.rios87@gmail.com>
2020-02-02 20:12:13 +0000

    Added basic tableview for the main menu usign diffable datasource

---
+++ RICCollectionViews/RICCollectionViews/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  RICCollectionViews
+//
+//  Created by Ricardo Ríos Arroyo on 02/02/2020.
+//  Copyright © 2020 Ricardo Ríos Arroyo. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
